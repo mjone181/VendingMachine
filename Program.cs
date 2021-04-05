a6c1654e54ebfc812525c29af277910622fdc7d1
mjone181 <mjone181@gmail.com>
2021-04-05 11:49:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introduction message welcoming User to the Application.
+            Console.WriteLine("Hello! Welcome to my Vending Machine Program, written in C#!");
+
+            //While Loop goes here
+            //Start of the loop. User will end up back here after every choice everntually.
+            Console.WriteLine("Please press any key to continue!");
+            Console.ReadLine();
+        }
+    }
+}
